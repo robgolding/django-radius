8fd681dbe7c640aa3ad82542a7551c858904b0d6
Rob Golding <rob@robgolding.com>
2011-05-03 00:11:05 +0100

    Update RADIUSBackend to use "realms"

--- radiusauth/backends/radius.py
+++ radiusauth/backends/radius.py
@@ -13,6 +13,8 @@ ATTRIBUTE User-Name     1 string
 ATTRIBUTE User-Password 2 string encrypt=1
 """
 
+REALM_SEPARATOR = '@'
+
 class RADIUSBackend(object):
     """
     Authenticate against a RADIUS server, specified by the following settings:
@@ -31,50 +33,68 @@ class RADIUSBackend(object):
     supports_anonymous_user = False
     supports_object_permissions = False
 
-    def __init__(self, server=None, port=None, secret=None):
-        self.server = server
-        self.port = port
-        self.secret = secret
-
-        if self.server is None:
-            self.server = settings.RADIUS_SERVER
-        if self.port is None:
-            self.port = settings.RADIUS_PORT
-        if self.secret is None:
-            self.secret = settings.RADIUS_SECRET
-        self.secret.encode('utf-8')
-
-        self.client = Client(server=self.server,
-                             authport=self.port,
-                             secret=self.secret,
-                             dict=Dictionary(StringIO(DICTIONARY)),
-                            )
-
     def _get_dictionary(self):
         return Dictionary(StringIO(DICTIONARY))
 
-    def _get_auth_packet(self, username, password):
-        pkt = self.client.CreateAuthPacket(code=AccessRequest,
-                                           User_Name=username)
+    def _get_auth_packet(self, username, password, client):
+        pkt = client.CreateAuthPacket(code=AccessRequest,
+                                      User_Name=username)
         pkt["User-Password"] = pkt.PwCrypt(password)
         return pkt
+
+    def _parse_username(self, username):
+        """
+        Get the server details to use for a given username, assumed to be in
+        the format <username@realm>.
+        """
+        user, _, realm = username.rpartition(REALM_SEPARATOR)
+        if not user:
+            return (username, None)
+        return (user, realm)
+
+
+    def get_server(self, realm):
+        """
+        Get the RADIUS server details for the given realm.
+        """
+        return (
+            settings.RADIUS_SERVER,
+            settings.RADIUS_PORT,
+            settings.RADIUS_SECRET
+        )
 
     def authenticate(self, username, password):
         """
         Check username against RADIUS server and return a User object or None.
         """
-        username = username.encode('utf-8')
+        user, realm = self._parse_username(username)
+
+        if not realm:
+            return None
+
+        server = self.get_server(realm)
+
+        if not server:
+            return None
+
+        client = Client(server=server[0],
+                        authport=server[1],
+                        secret=server[2],
+                        dict=Dictionary(StringIO(DICTIONARY)),
+                       )
+
+        user = user.encode('utf-8')
         password = password.encode('utf-8')
 
-        packet = self._get_auth_packet(username, password)
+        packet = self._get_auth_packet(user, password, client)
 
         logging.debug("Sending RADIUS authentication packet: %s" % packet)
 
         try:
-            reply = self.client.SendPacket(packet)
+            reply = client.SendPacket(packet)
         except Timeout, e:
             logging.error("RADIUS timeout occurred contacting %s:%s" % \
-                          (self.server, self.port)
+                          (server[0], server[1])
                          )
             return None
         except Exception, e:
@@ -96,8 +116,10 @@ class RADIUSBackend(object):
             user = User.objects.get(username=username)
         except User.DoesNotExist:
             user = User(username=username)
-            user.set_unusable_password()
-            user.save()
+
+        user.set_password(password)
+        user.save()
+
         return user
 
     def get_user(self, user_id):
